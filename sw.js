c57abfd89f1e72d7233a47c9b06dc662e287d46e
Njabulo <tnjabulo826@gmail.com>
2026-06-19 00:15:24 +0200

    Bump SW cache name to bust stale cached password

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
-const CACHE_NAME = "myday-v1";
+const CACHE_NAME = "myday-v2";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
